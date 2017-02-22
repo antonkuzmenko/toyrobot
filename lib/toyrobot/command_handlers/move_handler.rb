b3b9f217b9853f58cf041d9165456d307841c824
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2017-02-22 22:58:25 +0200

    Cleanup MoveHandler

--- lib/toyrobot/command_handlers/move_handler.rb
+++ lib/toyrobot/command_handlers/move_handler.rb
@@ -3,16 +3,6 @@ module Toyrobot
     class MoveHandler < BaseHandler
       def call
         game.robot.move_forward(Game::STEP_SIZE)
-        # prevent_falling_out_of_bounds
-      end
-
-      private
-
-      def prevent_falling_out_of_bounds
-        game.x = 0 if game.x == -1
-        game.y = 0 if game.y == -1
-        game.x = game.table_length if game.x > game.table_length
-        game.y = game.table_width if game.y > game.table_width
       end
     end
   end
