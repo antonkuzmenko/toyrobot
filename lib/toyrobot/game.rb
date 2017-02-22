3e416c31408e8ff1ea16649ed33ba47a6a693708
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2017-02-23 01:33:10 +0200

    Refactor Game

--- lib/toyrobot/game.rb
+++ lib/toyrobot/game.rb
@@ -11,17 +11,35 @@ module Toyrobot
     end
 
     def handle_command(command)
-      handler_name = command.class.name.split('::').last.gsub(/Command\z/, '')
+      handler_name = handler_name(command)
 
-      if CommandHandlers.const_defined?("#{handler_name}Handler", false)
-        handler_class = CommandHandlers.const_get("#{handler_name}Handler", false)
-        handler = handler_class.new(self)
-        (handler.method(:call).arity == 1) ? handler.call(command) : handler.call
-      end
+      return unless handler_exists?(handler_name)
+
+      handler = create_handler(handler_name)
+      expects_command?(handler) ? handler.call(command) : handler.call
     end
 
     def to_s
       robot.to_s
+    end
+
+    private
+
+    def handler_name(command)
+      command.class.name.split('::').last.gsub(/Command\z/, '')
+    end
+
+    def handler_exists?(handler_name)
+      CommandHandlers.const_defined?("#{handler_name}Handler", false)
+    end
+
+    def create_handler(handler_name)
+      handler_class = CommandHandlers.const_get("#{handler_name}Handler", false)
+      handler_class.new(self)
+    end
+
+    def expects_command?(handler)
+      handler.method(:call).arity == 1
     end
   end
 end
