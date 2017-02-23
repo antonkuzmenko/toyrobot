114fa435dcfbb2737c7b9be119bf912fc378526c
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2017-02-23 02:18:59 +0200

    Prettify tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,25 +4,27 @@ RSpec.describe Toyrobot::Game do
   let(:game) { described_class.new }
 
   describe '#handle_command' do
-    let(:move_command) { Toyrobot::Commands::MoveCommand.new }
-    let(:right_command) { Toyrobot::Commands::RightCommand.new }
-    let(:left_command) { Toyrobot::Commands::LeftCommand.new }
+    let(:commands) { Toyrobot::Commands }
+    let(:commands) { Toyrobot::Commands }
+    let(:move_command) { commands::MoveCommand.new }
+    let(:right_command) { commands::RightCommand.new }
+    let(:left_command) { commands::LeftCommand.new }
 
     it 'places the robot' do
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, 0, 'NORTH'))
+      game.handle_command(commands::PlaceCommand.new(0, 0, 'NORTH'))
       expect(game.to_s).to eq '0,0,NORTH'
     end
 
     it 'moves the robot forward' do
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, 0, 'NORTH'))
+      game.handle_command(commands::PlaceCommand.new(0, 0, 'NORTH'))
       game.handle_command(move_command)
       expect(game.to_s).to eq '0,1,NORTH'
       game.handle_command(move_command)
       expect(game.to_s).to eq '0,2,NORTH'
     end
 
-    it 'rotates the robot to the right' do
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, 0, 'NORTH'))
+    it 'turns the robot to the right' do
+      game.handle_command(commands::PlaceCommand.new(0, 0, 'NORTH'))
       game.handle_command(right_command)
       expect(game.to_s).to eq '0,0,EAST'
       game.handle_command(right_command)
@@ -33,8 +35,8 @@ RSpec.describe Toyrobot::Game do
       expect(game.to_s).to eq '0,0,NORTH'
     end
 
-    it 'rotates the robot to the left' do
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, 0, 'NORTH'))
+    it 'turns the robot to the left' do
+      game.handle_command(commands::PlaceCommand.new(0, 0, 'NORTH'))
       game.handle_command(left_command)
       expect(game.to_s).to eq '0,0,WEST'
       game.handle_command(left_command)
@@ -46,7 +48,7 @@ RSpec.describe Toyrobot::Game do
     end
 
     it 'moves the robot forward' do
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, 0, 'NORTH'))
+      game.handle_command(commands::PlaceCommand.new(0, 0, 'NORTH'))
       game.handle_command(move_command)
       game.handle_command(right_command)
       game.handle_command(move_command)
@@ -54,7 +56,7 @@ RSpec.describe Toyrobot::Game do
     end
 
     it 'does not fall out of bounds' do
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, 0, 'NORTH'))
+      game.handle_command(commands::PlaceCommand.new(0, 0, 'NORTH'))
       10.times { game.handle_command(move_command) }
       expect(game.to_s).to eq '0,4,NORTH'
       game.handle_command(right_command)
@@ -63,18 +65,18 @@ RSpec.describe Toyrobot::Game do
     end
 
     it 'does not accept PLACE with invalid coordinates' do
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(-1, 0, 'NORTH'))
+      game.handle_command(commands::PlaceCommand.new(-1, 0, 'NORTH'))
       expect(game.to_s).to eq 'Unplaced'
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, -1, 'NORTH'))
+      game.handle_command(commands::PlaceCommand.new(0, -1, 'NORTH'))
       expect(game.to_s).to eq 'Unplaced'
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(5, 0, 'NORTH'))
+      game.handle_command(commands::PlaceCommand.new(5, 0, 'NORTH'))
       expect(game.to_s).to eq 'Unplaced'
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, 5, 'NORTH'))
+      game.handle_command(commands::PlaceCommand.new(0, 5, 'NORTH'))
       expect(game.to_s).to eq 'Unplaced'
     end
 
-    it 'does not accept PLACE with invalid facing' do
-      game.handle_command(Toyrobot::Commands::PlaceCommand.new(0, 0, 'INVALID'))
+    it 'does not accept PLACE with invalid direction' do
+      game.handle_command(commands::PlaceCommand.new(0, 0, 'INVALID'))
       expect(game.to_s).to eq 'Unplaced'
     end
 
